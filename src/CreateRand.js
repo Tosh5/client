b55229b065688cdc24f6a59d7ea17beeafab3dd1
Tosh5 <toshiya.5601048.com@gmail.com>
2022-12-31 10:53:44 +0900

    12311053

--- src/CreateRand.js
+++ src/CreateRand.js
@@ -1,14 +1,14 @@
-// import React from 'react'
+import React from 'react'
 
-// function CreateRand() {
+function CreateRand() {
     
-//     var random = Math.random();
-//     // console.log(random)
-//   return (
-//     <div>
-//         <h1>{random}</h1>
-//     </div>
-//   )
-// }
+    var random = Math.random();
+    // console.log(random)
+  return (
+    <div>
+        <h1>{random}</h1>
+    </div>
+  )
+}
 
-// export default CreateRand
+export default CreateRand
