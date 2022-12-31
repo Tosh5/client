bfebe6f91812e6f284845b8b24bd850b50f009bf
Tosh5 <toshiya.5601048.com@gmail.com>
2022-12-31 10:19:30 +0900

    1211019

--- src/SendMyIndex.js
+++ src/SendMyIndex.js
@@ -1,36 +1,36 @@
-import React, { useEffect, useState } from "react";
-import io from "socket.io-client";
+// import React, { useEffect, useState } from "react";
+// import io from "socket.io-client";
 
-const async = require("async");
-const socket = io.connect("https://cheer-app-server1.onrender.com")
+// const async = require("async");
+// const socket = io.connect("https://cheer-app-server1.onrender.com")
 
-let myindex3 = 24
+// let myindex3 = 24
 
-const sendmyindex = async (props) =>{
-  console.log(`current myindex is...... ${props}`)
-  await socket.emit("send_myindex" , props)
-  console.log('ran sendTest')
-}
+// const sendmyindex = async (props) =>{
+//   console.log(`current myindex is...... ${props}`)
+//   await socket.emit("send_myindex" , props)
+//   console.log('ran sendTest')
+// }
 
 
-function SendMyIndex(props) {
+// function SendMyIndex(props) {
 
-  console.log(props.myindex)
+//   console.log(props.myindex)
 
-  useEffect(() => {
-    myindex3 = props.myindex
-  },[props.myindex])
+//   useEffect(() => {
+//     myindex3 = props.myindex
+//   },[props.myindex])
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      sendmyindex(myindex3)
-    }, 100);
-    return () => clearInterval(interval);
-  }, []);
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       sendmyindex(myindex3)
+//     }, 100);
+//     return () => clearInterval(interval);
+//   }, []);
 
-  return (
-    <div>MyIndex is : {myindex3}</div>
-  )
-}
+//   return (
+//     <div>MyIndex is : {myindex3}</div>
+//   )
+// }
 
-export default SendMyIndex
+// export default SendMyIndex
