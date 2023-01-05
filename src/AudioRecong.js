dbffcc4b9c2d21d690e78b2bac7f1e2d5ea7e09f
Tosh5 <toshiya.5601048.com@gmail.com>
2023-01-05 12:48:15 +0900

    イケイケ＋がんばれ

--- src/AudioRecong.js
+++ src/AudioRecong.js
@@ -17,12 +17,23 @@ const AudioRecong = () => {
   const { ganbareCount, setGanbareCount } = useContext(GanbareCount)
   // const { interim, setInterim } = useContext(InterimCount)
 
-  var count1 = ( transcript.match( /頑張れ/g ) || [] ).length ;
-  var count2 = ( transcript.match( /がんばれ/g ) || [] ).length ;
-  var count3 = ( transcript.match( /ガンバレ/g ) || [] ).length ;
-  var counts = count1+count2+count3
+  // var count1 = ( transcript.match( /頑張れ/g ) || [] ).length ;
+  // var count2 = ( transcript.match( /がんばれ/g ) || [] ).length ;
+  // var count3 = ( transcript.match( /ガンバレ/g ) || [] ).length ;
 
-  setGanbareCount(counts)
+  // var count4 = ( transcript.match( /いけ/g ) || [] ).length ;
+
+  var counts = []
+  counts = [...counts,( transcript.match( /頑張れ/g ) || [] ).length ]
+  counts = [...counts,( transcript.match( /がんばれ/g ) || [] ).length ]
+  counts = [...counts,( transcript.match( /いけ/g ) || [] ).length ]
+  counts = [...counts,( transcript.match( /池/g ) || [] ).length ]
+  counts = [...counts,( transcript.match( /イケ/g ) || [] ).length ]
+  
+
+  // var counts = count1+count2+count3+count4
+
+  setGanbareCount(counts.reduce((sum, element) => sum + element, 0))
 
   // var countInterim = ( interimTranscript.match( /頑張れ/g ) || [] ).length ;
   // setInterim(countInterim)
